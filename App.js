cf180bff97915d17255f5ae33b34eef8fea520e5
Sriram <srirammandalapu49@gmail.com>
2025-04-11 01:18:51 +0530

    commit

--- App.js
+++ App.js
@@ -1,14 +1,90 @@
-const parent = React.createElement("div", {id:"parent"}, [
-    React.createElement("div", {id: "children"},[
-        React.createElement("h1", {}, "I'm a H1 tag"),
-        React.createElement("h2", {}, "I'm a H2 tag"),]),
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-        React.createElement("div", {id: "children2"},[
-            React.createElement("h1", {}, "I'm a H1 tag"),
-            React.createElement("h2", {}, "I'm a H2 tag"),])
-]);
-const heading = React.createElement
+/*
+* Header
+*  - Logo
+*  - Nav Items
+* Body
+*  - Search
+*  - RestrauntContainer
+*    - RestrauntCard
+*      - Img
+*      - Rest name, rating, Cusinines, Delivery time
+* Footer
+*  - Copyright
+*  - Links
+*  - Address
+*  - Contact
+*/
 
-console.log(parent);
+const Header = () =>{
+    return(
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://i.pinimg.com/736x/b1/fc/bb/b1fcbbfd4fb8116c714ef352bb39bbaf.jpg' />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+ 
+const RestrauntCard = () =>{
+    return(
+        <div className='res-card' style={{
+            backgroundColor: '#f0f0f0'
+        }}>
+            <img 
+            className='res-logo'
+            alt='res-logo'
+            src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2024/11/21/395529a6-0759-432e-b44f-dd83cb6844b9_392699.jpg'
+            />
+            <h3>Nandana Palace</h3>
+            <h4>
+                Biryani, North-Indian, Asian
+            </h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    )
+}
+const Body = () =>{
+    return(
+        <div className='body'>
+            <div className='search'>
+                Search
+            </div>
+            <div className='res-container'>
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+               <RestrauntCard />
+            </div>
+        </div>
+    )
+}
+const AppLayout = () =>{
+    return <div className='app'>
+        <Header />
+        <Body />
+    </div>
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namaste React
+
+# Parcel
+  - Dev Build
+  - Local Server
+  - HMR -> Hot Module Replacement
+  - FIle watching Algorithm 
+  - Caching -> Faster Builds
+  - Image Optimisation 
+  - Minification 
+  - Bundling 
+  - Compress code 
+  - Code Splitting 
+  - Differentical Bundling -> to run smoothly in differnt browsers 
+  - Error Handling
+  - HTTPs
+  - Tree Shaking - Removes unused files
+  - Different dev and prod bundles 
